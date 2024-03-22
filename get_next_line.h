dcd7144e5f2ba8073418582e641a20da6332acff
Alicja Stefanska <alicja.stefanska44@gmail.com>
2024-03-22 17:01:57 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astefans <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 19:29:00 by astefans          #+#    #+#             */
+/*   Updated: 2024/03/22 15:59:19 by astefans         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+#endif
+
+char	*get_next_line(int fd);
+
+#endif
